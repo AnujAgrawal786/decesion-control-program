69f7b79dd229c778faa77d5f8093ddccff7ab821
AnujAgrawal786 <88234650+AnujAgrawal786@users.noreply.github.com>
2023-08-05 18:51:11 +0530

    Add files via upload

---
+++ negative.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a ;
+printf("enter negative number");
+scanf("%d",&a);
+    if(a<0)
+     {
+	 printf("%d",a);
+	 }
+	 else
+	 {
+	 printf ("please enter negative number");
+	 scanf("%d",&a);
+	 printf("%d",a);
+	 }
+	 
+ getch();
+	 
+}
