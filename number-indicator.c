700b463734d1ed89dd3cda2e8ec38ade5d9ee628
AnujAgrawal786 <88234650+AnujAgrawal786@users.noreply.github.com>
2023-08-05 18:57:20 +0530

    Add files via upload

---
+++ number-indicator.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int number;
+printf("enter number;");
+scanf("%d",&number);
+  if(number >= 0)
+  {
+  printf("positive number");
+  }
+  else
+   {
+  printf("negative number:\n");
+  
+     
+	   
+    }
+}	
+	
