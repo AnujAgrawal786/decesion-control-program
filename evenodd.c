8634479bd63e868eb408abfeb6b71719ca6c173d
AnujAgrawal786 <88234650+AnujAgrawal786@users.noreply.github.com>
2023-08-05 18:49:53 +0530

    Add files via upload

---
+++ evenodd.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n;
+printf("enter any number:");
+scanf("%d",&n);
+int c;
+c = n%2;
+ if(c = 0)
+  {
+  printf("even number:");
+  }
+  else
+  {
+  printf("odd number:");
+  }
+getch();
+ }
